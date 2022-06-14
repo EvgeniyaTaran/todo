e0f2dc0ce04139a2d9e0482af50cae07806e7fe1
Evgeniya Alekseyenko <evgeniya.alekseyenko@valtech.com>
2022-06-14 10:15:44 +0200

    Got rid of method in the template

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -25,7 +25,7 @@ export class TodoListComponent {
   public getTableName() {
     return this.isActive? "List Todo" : "Done";
   }
-  /////
+  
   TodoState = TodoStatus;
   ActionType = ActionType;
 
