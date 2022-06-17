9f4d8c7b8ed016f0b911af72c6562c877230b7c2
Evgeniya Alekseyenko <evgeniya.alekseyenko@valtech.com>
2022-06-17 11:44:40 +0200

    Updated app to work with mock server

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -3,31 +3,35 @@ import { Injectable } from "@angular/core";
 import { Todo } from "../models/todo";
 import { Observable } from 'rxjs';
 import { EditData } from "../models/todo-edit-data";
+import { map, mergeMap } from  'rxjs/operators';
+import { TodoState } from "../store/reducers/todos.reducer";
 
 @Injectable()
 export class TodoService {
     constructor(private http: HttpClient) {}
 
-    private url: string = "http://localhost:3000";
+    // private url: string = "http://localhost:3000";
 
     addTodo(payload: Todo): Observable<Todo> {
-        return this.http.post<Todo>(`${this.url}/todos`, payload);
+        return this.http.post<Todo>(`api/todos`, payload);
     }
 
     editTodo(payload: EditData): Observable<Todo> {
-        return this.http.put<Todo>(`${this.url}/todos/${payload.id}`, payload);
+        return this.http.put<Todo>(`api/todos/${payload.id}`, payload);
     }
 
     updateTodo(payload: Todo): Observable<Todo> {
-        return this.http.put<Todo>(`${this.url}/todos/${payload.id}`, payload);
+        return this.http.put<Todo>(`api/todos/${payload.id}`, payload);
     }
     
     removeTodo(payload: Todo): Observable<Todo> {
-        return this.http.delete<Todo>(`${this.url}/todos/${payload.id}`);
+        return this.http.delete<Todo>(`$api/todos/${payload.id}`);
     }
 
     getTodos(): Observable<Todo[]> {
-        return this.http.get<Todo[]>(`${this.url}/todos`);
+        return this.http.get<{data: Todo[]}>(`api/todos`).pipe(
+            map((response) => response.data)
+        );
     }
 }
 
